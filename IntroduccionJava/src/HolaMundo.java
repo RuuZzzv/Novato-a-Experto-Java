ad408606a3f1cac8ea6e92714007fcfc307aea9b
ruzvex <rs.daysuu@gmail.com>
2025-01-01 23:45:13 -0500

    Fundamentos básicos de Java

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    // Funcion principal(Main)
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con Java");
+    } // fin de la función
+}
---
+++ IntroduccionJava/src/Saludos.java
@@ -0,0 +1,11 @@
+public class Saludos {
+    public static void main(String[] args) {
+
+        //Nombre Completo
+        System.out.println("Nombre: Shande Daysuke Fernandez Rojas");
+        //Edad
+        System.out.println("Edad: 20 años");
+        //País
+        System.out.println("País: Perú");
+    }
+}
