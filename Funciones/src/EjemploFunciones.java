b930656cc213af844f454601eabfcd3d96be55a7
ruzvex <rs.daysuu@gmail.com>
2025-02-23 13:10:00 +0100

    Funciones - 12% - 23/02/2025

---
+++ Funciones/src/EjemploFunciones.java
@@ -0,0 +1,33 @@
+/**
+ * La clase EjemploFunciones ejemplifica el uso de funciones (métodos) en Java.
+ * Este ejemplo define una función estática llamada {@code saludar} que recibe un mensaje
+ * y lo imprime en la consola con un prefijo. El método {@code main} invoca esta función
+ * para demostrar su funcionamiento.
+ */
+public class EjemploFunciones {
+
+    /**
+     * Imprime un mensaje en la consola.
+     * 
+     * El método recibe un parámetro de tipo {@code String} que reprsesenta el mensaje a mostrar.
+     * Se utiliza {@code System.out.println} para imprimir el mensaje, precedido del prefijo "Mensaje: ".
+     * 
+     *
+     * @param mensaje El mensaje que se desea mostrar en la consola.
+     */
+    static void saludar(String mensaje) {
+        System.out.println("Mensaje: " + mensaje);
+    }
+
+    /**
+     * Método principal que inicia la ejecución del programa.
+     * 
+     * En este método se invoca a la función {@code saludar} pasando el mensaje "Hola".
+     * 
+     *
+     * @param args Argumentos de la línea de comandos (no se utilizan en este programa).
+     */
+    public static void main(String[] args) {
+        saludar("Hola");
+    }
+}
---
+++ Funciones/src/FuncionSumar.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * La clase FuncionSumar demuestra cómo crear y utilizar una función que suma dos números enteros.
+ * El programa solicita al usuario que ingrese dos valores enteros, llama a la función {@code sumar} para calcular la suma,
+ * y luego muestra el resultado junto con los valores ingresados.
+ */
+public class FuncionSumar {
+
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a el primer número a sumar.
+     * @param b el segundo número a sumar.
+     * @return la suma de {@code a} y {@code b}.
+     */
+    static int sumar(int a, int b) {
+        int resultado = a + b;
+        return resultado;
+    }
+
+    /**
+     * Método principal que ejecuta el programa.
+     * Este método realiza las siguientes acciones:
+     *   Solicita al usuario que ingrese dos números enteros.
+     *   Utiliza la función {@code sumar} para calcular la suma de dichos números.
+     *   Muestra el resultado de la suma y los valores ingresados en la consola.
+     * 
+     * 
+     *
+     * @param args argumentos de línea de comandos (no se utilizan en este programa).
+     */
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario desde la consola.
+        var consola = new Scanner(System.in);
+        int arg1, arg2;
+
+        // Solicitar al usuario el primer valor y convertirlo a entero.
+        System.out.print("Ingrese el valor A: ");
+        arg1 = Integer.parseInt(consola.nextLine());
+
+        // Solicitar al usuario el segundo valor y convertirlo a entero.
+        System.out.print("Ingrese el valor B: ");
+        arg2 = Integer.parseInt(consola.nextLine());
+
+        // Llamar a la función sumar para obtener la suma de arg1 y arg2.
+        var resultado = sumar(arg1, arg2);
+
+        // Mostrar en consola el resultado de la suma junto con los valores ingresados.
+        System.out.print("El resultado de la suma es: " + resultado
+                + "\nValores:"
+                + "\n" + arg1
+                + "\n" + arg2);
+        System.out.println("\n");
+    }
+}
